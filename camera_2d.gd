3e42667fa523b8ba8fae0c74e24615429b514505
Titan <68808479+titan3755@users.noreply.github.com>
2024-10-23 17:31:14 +0600

    added three body problem simulation

---
+++ camera_2d.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+@export var cam_velocity:float = 400.0
+@export var cam_zoom_velocity:float = 2.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	input_process(delta)
+	
+func input_process(del: float):
+	if Input.is_action_pressed("cam_up"):
+		position.y -= cam_velocity * del
+	if Input.is_action_pressed("cam_down"):
+		position.y += cam_velocity * del
+	if Input.is_action_pressed("cam_left"):
+		position.x -= cam_velocity * del
+	if Input.is_action_pressed("cam_right"):
+		position.x += cam_velocity * del
+	if Input.is_action_pressed("cam_zoom_in"):
+		zoom += Vector2(cam_zoom_velocity * del, cam_zoom_velocity * del)
+	if Input.is_action_pressed("cam_zoom_out"):
+		zoom -= Vector2(cam_zoom_velocity * del, cam_zoom_velocity * del)
--- celestial_object.gd
+++ celestial_object.gd
@@ -1,13 +1,12 @@
 extends Area2D
 
-@export var feffx:float = 0
-@export var feffy:float = 0
-@export var accx:float = 0
-@export var accy:float = 0
-@export var velx:float
-@export var vely:float
-@export var init_pos_x:float
-@export var init_pos_y:float
+@export var effective_force:Vector2 = Vector2(1, 1)
+@export var acceleration:Vector2 = Vector2(1, 1)
+@export var velocity:Vector2 = Vector2(1, 1)
+@export var velocity_clamp:float = 30.0
+@export var velocity_damping:float = 0.5
+@export var acceleration_clamp:float = 10.0
+@export var init_pos:Vector2
 @export var mass:float
 @export var radius:float
 @export var color:Color
@@ -17,9 +16,29 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
+	position = init_pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position += Vector2(velx, vely) * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	draw_circle(Vector2(init_pos_x, init_pos_y), radius, color, true, -1.0, true)
+	acceleration += (effective_force / mass) * delta
+	velocity += acceleration * delta
+	velocity.x = clamp(velocity.x, -velocity_clamp, velocity_clamp)
+	velocity.y = clamp(velocity.y, -velocity_clamp, velocity_clamp)
+	acceleration.x = clamp(acceleration.x, -acceleration_clamp, acceleration_clamp)
+	acceleration.y = clamp(acceleration.y, -acceleration_clamp, acceleration_clamp)
+	position += velocity * delta
+	damper(delta)
+	queue_redraw()
+
+func _draw() -> void:
+	draw_circle(position, radius, color, true, -1.0, true)
+	
+func damper(del: float):
+	if (velocity.x <= 0):
+		velocity.x += velocity_damping * del
+	if (velocity.x > 0):
+		velocity.x -= velocity_damping * del
+	if (velocity.y <= 0):
+		velocity.y += velocity_damping * del
+	if (velocity.y > 0):
+		velocity.y -= velocity_damping * del
